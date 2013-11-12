0fda67a4dfef7fdddd79b13eebe7fc4ad8e46038
djonker <djonker@oculusinfo.com>
2013-11-12 15:12:05 -0500

    release 1.0 updates

--- aperture/aperture-graph/src/main/java/oculus/aperture/graph/aggregation/impl/BasicGraphAggregationResult.java
+++ aperture/aperture-graph/src/main/java/oculus/aperture/graph/aggregation/impl/BasicGraphAggregationResult.java
@@ -1,11 +1,26 @@
 /**
- * Copyright (c) 2013 Oculus Info Inc. All rights reserved.
+ * Copyright (c) 2013 Oculus Info Inc.
+ * http://www.oculusinfo.com/
  *
- * This software is the confidential and proprietary information of
- * Oculus Info Inc. ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Oculus Info Inc.
+ * Released under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package oculus.aperture.graph.aggregation.impl;
 
